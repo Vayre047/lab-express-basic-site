41051f186a50983382526c0fbde4e1fbf89c5e43
Vayre047 <94454907+Vayre047@users.noreply.github.com>
2023-08-01 16:17:00 +0100

    Struture Done

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/work", (req, res) => {
+    res.sendFile(__dirname + "/views/work.html");
+});
+
+app.listen(3000, () => console.log("it's running on 3000 port"));
